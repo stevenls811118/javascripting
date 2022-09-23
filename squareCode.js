338bddc799bd450cd0808f6f28d96af539fce5cc
Yifan Shi <stevenls1118@msn.com>
2022-09-22 21:49:18 -0600

    move some files

--- squareCode.js
+++ squareCode.js
@@ -2,25 +2,25 @@ const squareCode = function(message) {
   // Put your solution here
   let noSpaceStr = message.replace(/ /g, "");
   let rowLength = Math.ceil(Math.sqrt(noSpaceStr.length));
-  let columnLength = Math.ceil(noSpaceStr.length/rowLength);
+  let columnLength = Math.ceil(noSpaceStr.length / rowLength);
   let array = noSpaceStr.split('');
 
-  for(let i = rowLength; i < array.length; i = i + rowLength + 1) {  
+  for (let i = rowLength; i < array.length; i = i + rowLength + 1) {
     array.splice(i, 0, '\n');
-  } 
+  }
 
   let squareStr = array.join("");
   let result = "";
 
-  for(let a = 0; a < rowLength; a++) {
-    for(let b = 0; b < columnLength; b++) {
-      if(a + (rowLength + 1) * b < squareStr.length) {
-        result += squareStr[a + (rowLength+1) * b];
+  for (let a = 0; a < rowLength; a++) {
+    for (let b = 0; b < columnLength; b++) {
+      if (a + (rowLength + 1) * b < squareStr.length) {
+        result += squareStr[a + (rowLength + 1) * b];
       }
     }
     result += " ";
   }
-  return result; 
+  return result;
 };
 
 console.log(squareCode("chill out"));
